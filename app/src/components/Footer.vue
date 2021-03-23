ad0eac801d1ec4d3d6f69fef7620357963206df2
Guilherme Costa <guilherme.costaa19@gmail.com>
2021-03-23 15:51:32 +0000

    Footer

---
+++ app/src/components/Footer.vue
@@ -0,0 +1,51 @@
+<template>
+    <div id="footer" class="mt-5">
+        <div class="row container-fluid">
+            <div class="col-4">
+                <h4 class="mb-3">Contacto</h4>
+                <p><b-icon-telephone-fill/>  +351 252 167 609</p>
+                <p><b-icon-envelope/>  geral@nauhouse.pt</p>
+                <p><b-icon-app/>  www.nauhouse.pt</p>
+            </div>
+            <div class="col-4">
+                <h4 class="mb-3">Livro de Reclamações</h4>
+                
+            </div>
+            <div class="col-4">
+                <h4 class="mb-3">Redes Sociais</h4>
+                <b-icon-facebook class="mr-2"></b-icon-facebook>
+                <b-icon-instagram class="mr-2"></b-icon-instagram>
+            </div>
+        </div>
+        <div class="row footerEnd container-fluid">
+            <div class="col-6">
+                <p>Copyright © 2018 NauHOUSE – by VIRAL | Consulting</p>
+            </div>
+            <div class="col-3">
+                <p>POLITICA DE PRIVACIDADE</p>
+            </div>
+            <div class="col-3">
+                <p>INFORMAÇÃO AO CONSUMIDOR</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+#footer .container-fluid {
+    background-color: #717171;
+}
+
+#footer .footerEnd {
+    width: 100%;
+    height: 50px;
+    background-color: #E2E2E2;
+    text-align: center;
+}
+</style>
