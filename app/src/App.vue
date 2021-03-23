4ea63ff6ec51eb40475582141d922830c76488ef
Guilherme Costa <guilherme.costaa19@gmail.com>
2021-03-23 15:50:58 +0000

    app updates

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,32 +1,38 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Navbar />
+    <router-view />
   </div>
 </template>
+
+<script>
+import Navbar from "@/components/Navbar.vue";
+export default {
+  components: {
+    Navbar,
+  },
+};
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  text-align: left;
 }
 
 #nav {
   padding: 30px;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.container {
+  margin-left: 50px;
+  margin-right: 50px;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.container-fluid {
+  width: 100%;
 }
+
 </style>
