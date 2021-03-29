66a7a1e000e36f13511be7103c2b608a36602558
Guilherme Costa <guilherme.costaa19@gmail.com>
2021-03-29 14:27:26 +0100

    import emailjs

--- app/src/main.js
+++ app/src/main.js
@@ -6,6 +6,8 @@ import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import VueSweetalert2 from 'vue-sweetalert2';
+import{ init } from 'emailjs-com';
+init("user_ERkgj4TuWJz3216R2nMuM");
 
 import 'sweetalert2/dist/sweetalert2.min.css';
 import 'bootstrap/dist/css/bootstrap.css';
